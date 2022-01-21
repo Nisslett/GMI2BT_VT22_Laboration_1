ef031ba46fdc794ff0717dca10da949c41433a63
Nisslett <Nisslett@users.noreply.github.com>
2022-01-21 17:20:45 +0100

    small fix

--- lab2/menu.py
+++ lab2/menu.py
@@ -2,7 +2,8 @@ from common import input_int_in_range,encapsulate
 import lab2.modules
 
 def menu() -> None:
-    """Main Laboration 2"""
+    """Menu Laboration 2"""
+    errortext="\nFelaktig inmatning, välj ett av ovanstående alternativ."
     while True:
         print(encapsulate(" Laboration 2 "))
         print("Välj ett av alternativen listade nedan.")
@@ -12,7 +13,7 @@ def menu() -> None:
         print("4. Ta bort person")
         print("5. Spara fil")
         print("6. Återgå till huvudmeny")
-        
+        menu_input=input_int_in_range("Välj ett meny alternativ:",1,3,errortext)
         if menu_input == 1:
             pass
         elif menu_input == 2:
